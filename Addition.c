32f935a76c3f2e1831f093652944af0b30929ebd
Kedar Dhumal <kedardhumal31@gmail.com>
2024-01-16 23:20:09 +0530

    LB

---
+++ Addition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,Sum;
+	
+	printf("Enter the number : \n");
+	scanf("%d",&a);
+	
+	printf("Enter the number : \n");
+	scanf("%d",&b);
+	
+	Sum = a+b;
+	
+	printf("Addition is : %d ",Sum);
+	
+	return 0;
+	
+}
