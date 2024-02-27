5598609fd01b3c6e8cc5a039f3b02f7bec7cc269
Kedar Dhumal <kedardhumal31@gmail.com>
2024-02-27 23:08:44 +0530

    Program

---
+++ DivisibleByFive.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int DivisibleByFive(int iNo)
+{
+	int iAns = 0;
+	
+	iAns = iNo % 5;
+	
+	printf("value of iAns = %d \n",iAns);
+	
+	if(iAns == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int main()
+{
+	int iValue = 0, iRet = 0;
+	
+	printf("Enter a number : \n");
+	scanf("%d",&iValue);
+	
+	iRet = DivisibleByFive(iValue);
+	
+	if(iRet == 0)
+	{
+		printf("%d is not divisible by 5", iValue);
+	}
+	else
+	{
+		printf("%d is divisible by 5", iValue);
+	}
+	
+	return 0;
+	
+}
+
